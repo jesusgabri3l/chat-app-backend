9f3cd1b3639c58c997713e6f6020670518c1612c
Jesús Gabriel <jesus15202009@gmail.com>
2022-01-10 21:53:08 -0500

    fix

--- src/controllers/messagesController.ts
+++ src/controllers/messagesController.ts
@@ -2,7 +2,10 @@ import {Response, Request} from 'express';
 const { getMessages: getAllMessagesHandler } = require('./firebase/firebase');
 
 const getAllMessages = async (req: Request, res: Response) => {
-    if (res.statusCode === 403) res.send('Invalid credentials'); return;
+    if (res.statusCode === 403) {
+        res.send('Invalid credentials'); 
+        return;
+    } 
     const messages = await getAllMessagesHandler();
     if(messages) res.send(messages);
     else res.status(500);
